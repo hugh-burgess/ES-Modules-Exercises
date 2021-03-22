09c84476e9aa584076e126c92f4ef4fc53b9cc0f
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-22 12:41:03 +0100

    done

---
+++ 1-es-modules-syntax/lib/math.js
@@ -0,0 +1,17 @@
+export default function sum(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+export { substract, multiply, divide };
--- 1-es-modules-syntax/main.js
+++ 1-es-modules-syntax/main.js
@@ -1,20 +1,6 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+import sum, { substract as takeaway, multiply, divide } from "./lib/math.js";
 
 console.log(sum(4, 5));
-console.log(substract(8, 4));
+console.log(takeaway(8, 4));
 console.log(multiply(6, 4));
 console.log(divide(8, 2));
